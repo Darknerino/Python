625c4a643c5841325e5bfbe25251e6bd2b59addb
Darknerino <70180329+Darknerino@users.noreply.github.com>
2021-09-22 11:19:05 +0200

    Add files via upload

---
+++ wiek_sredni.py
@@ -0,0 +1,8 @@
+S = int(input("Podaj srenia wieku: "))
+X = int(input("Podaj wiek osoby a: "))
+Y = int(input("Podaj wiek osoby b: "))
+
+wiek = S * 3 - X - Y
+wiek = str(wiek)
+
+print("Wiek trzeciej osoby to: " + wiek)
